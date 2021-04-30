6d5f92ef55f2cce22acd227f430a994b05aa40ac
Diptiman1999 <diptiman.sen1999@gmail.com>
2021-04-30 23:26:52 +0530

    Static Analysis Done Successfully.

--- main.py
+++ main.py
@@ -2,21 +2,30 @@
 import random
 import time
 
-# Initial Steps to invite in the game:
+# Variables
+count = 0
+display = ""
+word = ""
+already_guessed = []
+play_again = ""
+
+# Displaying the Username and Instructions to play the game
 print("\n-----------------Welcome to Hangman game--------------------\n")
 name = input("Enter your name: ")
 print("Hello " + name + "! Best of Luck!")
 time.sleep(2)
 print("\nInstructions:")
-print("1.You have to guess the word of a particular category given")
-print("2.You will have 6 lives. For each incorrect guess it will lead to loss of one life")
+print("1.You have to guess the word of a particular category given.")
+print("2.You will have six lives. For each incorrect guess, it will lead to the loss of one life.")
 time.sleep(5)
 input("\nPress Enter to START!")
 print("\nThe game is about to start!")
 print("Let's play Hangman!")
 time.sleep(3)
 
-def draw_man(count):
+
+# Drawing the Pattern depending upon the incorrect answers
+def draw_man(progress):
     #    _____
     #   |     |
     #   |     |
@@ -26,99 +35,99 @@ def draw_man(count):
     #   |    / \
     # __|__
 
-
-    if count == 1:
+    if progress == 1:
         time.sleep(1)
-        print("         \n"
-              "  |      \n"
-              "  |      \n"
-              "  |      \n"
-              "  |      \n"
-              "  |      \n"
-              "  |      \n"
+        print("         \n" +
+              "  |      \n" +
+              "  |      \n" +
+              "  |      \n" +
+              "  |      \n" +
+              "  |      \n" +
+              "  |      \n" +
               "__|__\n")
 
-    if count == 2:
+    if progress == 2:
         time.sleep(1)
-        print("   _____ \n"
-              "  |      \n"
-              "  |      \n"
-              "  |      \n"
-              "  |      \n"
-              "  |      \n"
-              "  |      \n"
+        print("   _____ \n" +
+              "  |      \n" +
+              "  |      \n" +
+              "  |      \n" +
+              "  |      \n" +
+              "  |      \n" +
+              "  |      \n" +
               "__|__\n")
 
-
-    elif count == 3:
+    elif progress == 3:
         time.sleep(1)
-        print("   _____ \n"
-              "  |     | \n"
-              "  |     |\n"
-              "  |      \n"
-              "  |      \n"
-              "  |      \n"
-              "  |      \n"
+        print("   _____ \n" +
+              "  |     | \n" +
+              "  |     |\n" +
+              "  |      \n" +
+              "  |      \n" +
+              "  |      \n" +
+              "  |      \n" +
               "__|__\n")
 
-
-    elif count == 4:
+    elif progress == 4:
         time.sleep(1)
-        print("   _____ \n"
-              "  |     | \n"
-              "  |     |\n"
-              "  |     | \n"
-              "  |      \n"
-              "  |      \n"
-              "  |      \n"
+        print("   _____ \n" +
+              "  |     | \n" +
+              "  |     |\n" +
+              "  |     | \n" +
+              "  |      \n" +
+              "  |      \n" +
+              "  |      \n" +
               "__|__\n")
 
-    elif count == 5:
+    elif progress == 5:
         time.sleep(1)
-        print("   _____ \n"
-              "  |     | \n"
-              "  |     |\n"
-              "  |     | \n"
-              "  |     O \n"
-              "  |      \n"
-              "  |      \n"
+        print("   _____ \n" +
+              "  |     | \n" +
+              "  |     |\n" +
+              "  |     | \n" +
+              "  |     O \n" +
+              "  |      \n" +
+              "  |      \n" +
               "__|__\n")
 
-    elif count == 6:
+    elif progress == 6:
         time.sleep(1)
-        print("   _____ \n"
-              "  |     | \n"
-              "  |     |\n"
-              "  |     | \n"
-              "  |     O \n"
-              "  |    /|\ \n"
-              "  |    / \ \n"
+        print("   _____ \n" +
+              "  |     | \n" +
+              "  |     |\n" +
+              "  |     | \n" +
+              "  |     O \n" +
+              "  |    /|\ \n" +
+              "  |    / \ \n" +
               "__|__\n")
     return
 
 
+# Main Function where initialization and random word chosen to be guessed
 def main():
     global count
     global display
     global word
     global already_guessed
-    global length
     global play_again
 
+    # Word stored depending upon their types
     category = {"Color": ["green", "pink", "lemon", "orange", "violet", "yellow", "indigo", "blue", "purple"],
                 "Language": ["Python", "Spanish", "Java", "English", "HTML", "Chinese", "Telugu", "Indonesian"],
                 "Animal": ["tiger", "lion", "jackal", "elephant", "kangaroo", "snake"]}
     category_choice = random.choice(list(category.keys()))
+
+    # Printing the category of word to be guessed
     print("\nCategory of the word you need to guess: " + category_choice)
     word = random.choice(category[category_choice])
-    length = len(word)
     count = 0
-    display = '_' * length
+    display = '_' * len(word)
     already_guessed = []
     play_again = ""
     hangman()
 
 
+# Asking user to play again or leave the game
 def play_loop():
     global play_again
     play_again = input("\nWant to play Again? Press y or Y for YES and n or N for NO :")
@@ -130,6 +139,7 @@ def play_loop():
         exit()
 
 
+# Hangman Function Used to check correctness of guessed letter
 def hangman():
     word_progress = ["-"] * len(word)
 
@@ -145,7 +155,7 @@ def hangman():
         # draw_man function used to display the DESIGN
 
         print(" ".join(word_progress))
-        if len(guessed)!=0:
+        if len(guessed) != 0:
             print("\nGuessed letters: ")
             print(" ".join(guessed))
         print()
@@ -161,7 +171,7 @@ def hangman():
 
         # You Have entered word instead of letter
         elif len(guess) > 1:
-            print("Sorry, that's not the letter. You have enter a word ")
+            print("Sorry, that's not the letter. You have entered a word ")
             incorrect += 1
             draw_man(incorrect)
 
@@ -202,5 +212,5 @@ def hangman():
     play_loop()
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     main()
